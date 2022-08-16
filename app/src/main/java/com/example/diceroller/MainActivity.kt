6e57e5c57fcbb696f8495b5814071802a23deb4a
MillaMachado <camillmachado@hotmail.com>
2022-08-16 18:42:16 -0300

    atualização de imagem

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -12,35 +13,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener { rollDice() }
+
+        rollDice()
     }
 
-    /** Esta function instancia um Dice com 6 numSides,
-     *  chama o método roll do Dice,
-     *  seleciona a TextView no layout
-     *  e atribui a ele o resultado do método.
+    /**
+     * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
-        val dice1 = Dice(6)
-        val dice2 = Dice(6)
-
-        val diceRoll1 = dice1.roll()
-        val diceRoll2 = dice2.roll()
-
-        val resultTextView1: TextView = findViewById(R.id.textView)
-        val resultTextView2: TextView = findViewById(R.id.textView2)
-
-        resultTextView1.text = diceRoll1.toString()
-        resultTextView2.text = diceRoll2.toString()
+        // Create new Dice object with 6 sides and roll the dice
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
 
 class Dice(val numSides: Int) {
 
-    /** Este método permite jogar o dado
-     * e obter um número randomico
-     * de 1 até numSIdes que é recebido por parâmetro.
-     */
     fun roll(): Int {
         return (1..numSides).random()
     }
